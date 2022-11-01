2480a6eedb14bbf6b14985ad8493eddefd63d182
Monica Barrios <49417323+MoniBarrios@users.noreply.github.com>
2022-11-01 15:01:37 -0700

    Updated pod file, Added homepage Views

---
+++ PestTech/PestTech/ListCell.swift
@@ -0,0 +1,23 @@
+//
+//  ListCell.swift
+//  PestTech
+//
+//  Created by Monica Barrios on 11/1/22.
+//
+
+import UIKit
+
+class ListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
