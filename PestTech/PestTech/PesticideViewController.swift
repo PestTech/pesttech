b99b2f2d62be35274976c93e8ca1814fad9e2a52
pere2332 <47095095+pere2332@users.noreply.github.com>
2022-11-29 16:10:40 -0800

    First Commint

---
+++ PestTech/PestTech/PesticideViewController.swift
@@ -0,0 +1,56 @@
+//
+//  PesticideViewController.swift
+//  PestTech
+//
+//  Created by Isidro  Perez on 11/16/22.
+//
+
+import UIKit
+import Parse
+
+class PesticideViewController: UIViewController {
+    
+    @IBOutlet weak var locationText: UITextField!
+    
+    @IBOutlet weak var dateMonth: UITextField!
+    @IBOutlet weak var dateDay: UITextField!
+    @IBOutlet weak var dateYear: UITextField!
+    
+    @IBOutlet weak var appType: UITextField!
+    
+    @IBOutlet weak var commondityTreat: UITextField!
+    
+    @IBOutlet weak var treatedArea: UITextField!
+    
+    @IBOutlet weak var pesticideName: UITextField!
+    
+    @IBOutlet weak var epaNumber: UITextField!
+    
+    @IBOutlet weak var measuringUnit: UITextField!
+    @IBOutlet weak var rateUnit: UITextField!
+    
+    @IBOutlet weak var dilution: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
